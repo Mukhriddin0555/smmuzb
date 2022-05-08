ef3f4037a841d2db66eb2f299a88903c96847ba4
Mukhriddin0555 <romantik4350555@gmail.com>
2022-05-08 12:11:34 +0500

    ssqqwww

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('sendmessage', [telegramController::class, 'sendmessage'])
+Route::get('/sendmessage', [telegramController::class, 'sendmessage'])
                 ->name('sendmessage');
-Route::get('ssmmalumot', [telegramController::class, 'sendmessage'])
+Route::get('/ssmmalumot', [telegramController::class, 'getmessage'])
                 ->name('sendmessage');
 Route::get('/', function () {
     return view('welcome');
