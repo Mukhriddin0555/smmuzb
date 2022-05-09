69db5113c4fd393734678a61fa267269c3ccc939
Mukhriddin0555 <romantik4350555@gmail.com>
2022-05-09 14:02:26 +0500

    sqert

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -118,8 +118,7 @@ class telegramController extends Controller
         ];
         $telegram->sendButtons($telegram_id, $this->textagree, $menu2);
     }
-    public function sendContactVerify(Telegram $telegram){
-            $chat_id = 34764210;
+    public function sendContactVerify($chat_id, $telegram){
             $text = $this->textveryfication;
             $button = [
                     'keyboard' =>
@@ -200,7 +199,7 @@ class telegramController extends Controller
             $message = $telegram->sendButtons($chat_id, $this->textagree, $button);
             $message = json_decode($message);
             $verify = new Veryfication();
-            $verify->message_id = $message->result->message_id;;
+            $verify->message_id = $message->result->message_id;
             $verify->chat_id = $chat_id;
             $verify->save();
         
