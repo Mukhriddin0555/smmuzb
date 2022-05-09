402da615ccd6d35246f1c25e9fb73470bb14a541
Muxriddin <romantik4350555@gmail.com>
2022-05-09 05:33:57 +0500

    ssnew

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -13,15 +13,15 @@ class telegramController extends Controller
     
     public function getmessage(Request $request, Telegram $telegram)
     {
-        $chat_id = $request['message']['chat']['id'];
-        $text = $request['message']['text'];
-        $from_id = $request['message']['from']['id'];
+        //$chat_id = $request['message']['chat']['id'];
+        //$text = $request['message']['text'];
+        //$from_id = $request['message']['from']['id'];
         //$first_name = $request['message']['from']['first_name'];
         //$last_name = $request['message']['from']['last_name'];
         //$username = $request['message']['from']['username'];
         //$chat_id = 34764210;
         //$message = '+998914885559';
-        if($text == 'test'){
+        /*if($text == 'test'){
             $chat_id = 34764210;
             $text = 'test';
             $button = [
@@ -38,8 +38,24 @@ class telegramController extends Controller
                 ];
             return $telegram->sendButtons($chat_id, $text, $button);
         }else{
-            Log::debug();
-        }
+            
+        }*/
+        Log::debug($request->all());
+        $chat_id = 34764210;
+            $text = 'test';
+            $button = [
+                    'keyboard' =>
+                    [
+                        [
+                            [
+                                'text' => 'Отправить свой контакт',
+                                'request_contact' => true,
+                            ]
+                        ]
+                    ],
+                    'one_time_keyboard' => true,
+                ];
+            return $telegram->sendButtons($chat_id, $text, $button);
     }
     public function sendmessage(Telegram $telegram)
         {
