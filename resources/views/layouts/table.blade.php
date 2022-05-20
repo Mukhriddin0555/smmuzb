6e417d05deacbbd4c4fa9922de542d916a947f77
Muxriddin <romantik4350555@gmail.com>
2022-05-20 07:26:30 +0500

    wwws

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -10,7 +10,7 @@
             <div class="flex justify-center font-bold">Савдо хакида малумот кушиш</div>
             <br>
             <div class="flex justify-center m-3 ml-8 mb-6">
-                <input class="rounded-l-lg p-4 border mr-0 text-gray-800 border-gray-200 bg-white" name="amount" type="number" placeholder="100000"/>
+                <input class="rounded-l-lg p-4 border mr-0 text-gray-800 border-gray-200 bg-white" name="amount" type="number" placeholder="Савдо суммаси"/>
                 <button class="px-8 rounded bg-yellow-400  text-gray-800 font-bold p-4 border-yellow-500 border">Кушиш</button>
             </div>
             
