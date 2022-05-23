896b47b210ec0d94c9a69c5eb015fb58dd817ab7
Mukhriddin0555 <romantik4350555@gmail.com>
2022-05-23 16:54:15 +0500

    sqweasdq

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -49,12 +49,12 @@ class telegramController extends Controller
         $last_name = 'Biznes';
         $user_id = $contact['user_id'];
         if(isset($contact['first_name'])){
-            if(!preg_match("%\\\%", $str)){
+            if(!preg_match("%\\\%", $contact['first_name'])){
                 $first_name = $contact['first_name'];
             }
         }
         if(isset($contact['last_name'])){
-            if(!preg_match("%\\\%", $str)){
+            if(!preg_match("%\\\%", $contact['last_name'])){
                 $last_name = $contact['last_name'];
             }
         }
