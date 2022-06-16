b473b2d5071cb7ffbf67f4d20e9234ebd431ebd0
Mukhriddin0555 <romantik4350555@gmail.com>
2022-06-16 15:41:39 +0500

    ssqqww

--- app/Http/Controllers/telegramController.php
+++ app/Http/Controllers/telegramController.php
@@ -407,7 +407,7 @@ class telegramController extends Controller
 
     }
     public function usersendcontact(Request $req, Telegram $telegram){
-        $chat_id = 1089386501;
+        $chat_id = 1859848777;
         $text = "SMMUZB.UZ Сайтидан навбатдаги мурожаат:
         Мурожаатчи: $req->name
         Тел. раками: $req->number
